53011a645e6b6f857aceca762f95bbc6b5718d97
wceballos <wceballos@init1.dev>
2021-12-10 01:29:33 -0700

    Add tip calculator app

---
+++ TipTime/README.md
@@ -0,0 +1,22 @@
+# Tip Time App
+App \#1 in Unit 2: Layouts.
+
+## Introduction
+An Android app that calculates a tip based on the cost and tip
+percentage from user input.
+
+## Learning Outcomes
+- Use `EditText`, `TextView`, `RadioGroup`, `RadioButton`, and `Switch`,
+  and `ScrollView`.
+- Build a UI layout using XML
+- Define styles and dimension resources
+- Create a light and dark theme according to *Material Design* guidelines.
+- Polish the UI using *Material Design Components*.
+- Use in-app icons whose colors adapt to the light/dark theme.
+- Use *Image Asset Studio* in Android studio to generate icon assets.
+- Create an adaptive app launcher icon for different screen densities.
+- Setup Gradle to use *view binding* instead of `findViewById()`.
+- Handle key press events.
+- Debug an app using *Logcat*.
+- Use *code inspection* to follow best practices.
+
---
+++ TipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021 Wilmin Ceballos
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.tiptime
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            calculateTip()
+        }
+
+        binding.costOfServiceEditText.setOnKeyListener {
+                view, keyCode, _ -> handleKeyEvent(view, keyCode)
+        }
+    }
+
+    private fun calculateTip() {
+        // Get the cost entered
+        val stringInTextField = binding.costOfServiceEditText.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        // Get the tip amount selected
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        // Calculate the tip
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip: Double) {
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+}
