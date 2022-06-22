6d54ea3bc078663c3b8c0f6fa18a2a8fd5838540
Wilmin Ceballos <wceballos@init1.dev>
2022-06-22 16:53:21 -0400

    Add Amphibians app

---
+++ Amphibians/README.md
@@ -0,0 +1,21 @@
+# Amphibians app
+App \#2 in Unit 4: Connect to the Internet
+
+## Introduction
+
+This Amphibians app contains a list of amphibians and details about them. The
+app demonstrates everything learned in Unit 4: connecting to the internet and
+REST concepts with
+[Retrofit](https://square.github.io/retrofit/) and
+[Moshi](https://github.com/square/moshi/) to connect to a RESTful service and
+parse JSON data.
+
+The app also leverages Kotlin Coroutines for network requests,
+[ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel),
+[LiveData](https://developer.android.com/topic/libraries/architecture/livedata), and
+[Data Binding](https://developer.android.com/topic/libraries/data-binding/)
+with binding adapters.
+
+This app is based off the [starter code](https://github.com/google-developer-training/android-basics-kotlin-amphibians-app/tree/main)
+for the Amphibians app in Android Basics [Unit 4](https://developer.android.com/courses/android-basics-kotlin/unit-4).
+The automated tests were provided by the starter code and not altered.
---
+++ Amphibians/app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.amphibians.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
+
+val moshi: Moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+val retrofit: Retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * A public interface that exposes the Amphibian API methods.
+ */
+interface AmphibianApiService {
+    @GET("android-basics-kotlin-unit-4-pathway-2-project-api.json")
+    suspend fun getAmphibians(): List<Amphibian>
+}
+
+/**
+ * A public API singleton to expose a lazy-initialized [AmphibianApiService]
+ */
+object AmphibianApi {
+    val retrofitService: AmphibianApiService
+        by lazy { retrofit.create(AmphibianApiService::class.java) }
+}
+
